c5c7187c3096ae3050f2b7771f46ad3a6c01cc26
Adel Ansari <adelansari@github.com>
2022-12-18 15:36:01 +0200

    first commit

---
+++ 2022/December/daily-temperatures/mySolution.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func dailyTemperatures(temperatures []int) []int {
+
+	tempArrayLength := len(temperatures)
+	tempWaitOutput := make([]int, tempArrayLength) // creating an array the same size as temperatures
+	for index := range temperatures {
+		if index+1 < tempArrayLength {
+			for j := index + 1; j < tempArrayLength; j++ {
+				if temperatures[j] > temperatures[index] {
+					tempWaitOutput[index] = j - index
+					break
+				}
+			}
+		} else {
+			tempWaitOutput[index] = 0
+		}
+	}
+	return tempWaitOutput
+}
+
+func main() {
+
+	fmt.Println(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
+	fmt.Println(dailyTemperatures([]int{30, 40, 50, 60}))
+	fmt.Println(dailyTemperatures([]int{30, 60, 90}))
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Leetcode Daily Challenges
+
+This repo will be dedicated to all the challenges I attempt in leetcode.
